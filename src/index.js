5e85693aeb83559566dca6e146af98ee036647f1
nicolasDEROUINEAU <nicolas.derouineau@delair.aero>
2019-11-26 17:32:30 +0100

    Animate every objects

--- src/index.js
+++ src/index.js
@@ -4,17 +4,18 @@ import GLTFLoader from 'three-gltf-loader';
 const OrbitControls = require('three-orbitcontrols')
 
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 0.1, 10000000);
 
 var renderer = new THREE.WebGLRenderer();
 renderer.gammaOutput = true;
 renderer.gammaFactor = 2.2;
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-var controls = new OrbitControls( camera, renderer.domElement );
+var controls = new OrbitControls(camera, renderer.domElement);
 
 camera.position.z = 2;
+camera.position.y = 500;
 controls.update();
 
 // Instantiate a loader
@@ -25,58 +26,60 @@ var clock = new THREE.Clock();
 
 // Load a glTF resource
 loader.load(
-	// resource URL
-	'Nico.glb',
-	// called when the resource is loaded
-	function ( gltf ) {
-		var model = gltf.scene;
+    // resource URL
+    'PlanetarySystem.glb',
+    // called when the resource is loaded
+    function (gltf) {
+        var model = gltf.scene;
 
-		scene.add(model );
+        scene.add(model);
 
-		gltf.animations; // Array<THREE.AnimationClip>
-		gltf.scene; // THREE.Scene
-		gltf.scenes; // Array<THREE.Scene>
-		gltf.cameras; // Array<THREE.Camera>
-		gltf.asset; // Object
-		
+        gltf.animations; // Array<THREE.AnimationClip>
+        gltf.scene; // THREE.Scene
+        gltf.scenes; // Array<THREE.Scene>
+        gltf.cameras; // Array<THREE.Camera>
+        gltf.asset; // Object
 
-		mixer = new THREE.AnimationMixer(model);
-		if(gltf.animations[0]){
-			mixer.clipAction(gltf.animations[0]).play();
-			animate();
-		}
-        
 
-	},
-	// called while loading is progressing
-	function ( xhr ) {
+        mixer = new THREE.AnimationMixer(model);
+        //if (gltf.animations[0]) {
 
-		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
 
-	},
-	// called when loading has errors
-	function ( error ) {
+        gltf.animations.forEach(function (item) {
+            mixer.clipAction(item).play();
+            animate();
+        });
 
-		console.log( 'An error happened' );
+    },
+    // called while loading is progressing
+    function (xhr) {
 
-	}
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+
+    },
+    // called when loading has errors
+    function (error) {
+
+        console.log('An error happened');
+        console.log(error);
+
+    }
 );
 
-var pointLight = new THREE.PointLight(0xffffff, 1, 100);
-pointLight.position.set(-5, 1, 5);
+var pointLight = new THREE.PointLight(0xffffff, 1, 10000000);
+pointLight.position.set(0, 0, 0);
 scene.add(pointLight);
 
 
 var animate = function () {
-	requestAnimationFrame( animate );
-	var delta = clock.getDelta();
-	if (mixer != null) {
-		mixer.update(delta);
-	};
-	renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+    var delta = clock.getDelta();
+    if (mixer != null) {
+        mixer.update(delta);
+    };
+    renderer.render(scene, camera);
 
 
-    renderer.render( scene, camera );
 };
 
 animate();
